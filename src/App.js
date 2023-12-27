864503776010436a9f74e2fe25b3f5a241e14aea
aditya81788 <aditya.singh99111@gmail.com>
2023-12-27 13:10:10 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoginSignUp from './Components/LoginSignUp/LoginSignup';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginSignUp />
+    </div>
+
+  );
+}
+
+export default App;
